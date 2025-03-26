b67683aa3490789762d017c08185c1f0f8b52863
Mehdi <149390266+mehdihz2003@users.noreply.github.com>
2025-03-26 12:56:55 +0100

    removed comments

--- src/modules/profanity.js
+++ src/modules/profanity.js
@@ -4,7 +4,3 @@ export function censorBadWords(text) {
     const regex = new RegExp(`\\b(${profanityList.join("|")})\\b`, "gi");
     return text.replace(regex, (match) => "*".repeat(match.length));
 }
-
-// Example usage:
-// console.log(censorBadWords("This is a damn example."));
-// Output: "This is a **** example."
