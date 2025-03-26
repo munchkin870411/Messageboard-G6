d65e81206aec8cbc87189a531e38ae2b16a8ad05
Mehdi <149390266+mehdihz2003@users.noreply.github.com>
2025-03-26 14:25:14 +0100

    messages gets displayed, including new ones

--- src/modules/main.js
+++ src/modules/main.js
@@ -1,15 +1,26 @@
-import { censorBadWords } from "./profanity.js";
 import { addMessageToFirebase, fetchMessagesFromFirebase } from "./firebase.js";
+import { displayMessages } from "./display.js";
+import { censorBadWords } from "./profanity.js";
+
+fetchMessagesFromFirebase().then(messagesArray => {
+    displayMessages(messagesArray);
+});
 
 const messageForm = document.querySelector("#messageForm");
-messageForm.addEventListener('submit', event => {
+messageForm.addEventListener('submit', async event => {
     event.preventDefault();
 
-  const formData = new FormData(event.target);
-  const userName = formData.get('name'); 
-  const userMessage = formData.get('message');
+    const formData = new FormData(event.target);
+    const userName = formData.get('name'); 
+    const userMessage = censorBadWords(formData.get('message'));
 
-  addMessageToFirebase(userMessage, userName);
+    try {
+        await addMessageToFirebase(userMessage, userName);
+        const updatedMessages = await fetchMessagesFromFirebase();
+        displayMessages(updatedMessages);
 
-  event.target.reset();
-})
+        event.target.reset();
+    } catch (error) {
+        console.error("Error adding message:", error);
+    }
+});
